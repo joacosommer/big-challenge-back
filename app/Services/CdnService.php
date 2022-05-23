e7b19c73a51aceb865b46e0d03dff77cba2b4026
Joaquin Sommer <joaquin.sommer@lightit.io>
2022-05-23 14:58:26 -0300

    Doctor prescription file store, update and delete

---
+++ app/Services/CdnService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Class CdnService.
+ */
+class CdnService
+{
+
+}
---
+++ tests/Feature/PrescriptionFileStoreTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Submission;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class PrescriptionFileUploadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function doctor_can_upload_prescription_file()
+    {
+        $this->withoutExceptionHandling();
+        $submission = Submission::factory()->inProgress()->create();
+        $this->actingAs($submission->doctor);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertStatus(200);
+        Storage::assertExists('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_guest_cannot_upload_prescription_file()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertUnauthorized();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_doctor_cannot_upload_prescription_file_for_submission_that_is_not_in_progress()
+    {
+        $submission = Submission::factory()->done()->create();
+        $this->actingAs($submission->doctor);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertForbidden();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_doctor_cannot_upload_prescription_file_for_submission_that_is_not_from_him()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        $doctor = User::factory()->doctor()->create();
+        $this->actingAs($doctor);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertForbidden();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_doctor_cannot_upload_prescription_file_for_submission_that_does_not_exist()
+    {
+        $this->actingAs(User::factory()->doctor()->create());
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . 999, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertNotFound();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_patient_cannot_upload_prescription_file()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        $this->actingAs($submission->patient);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.txt'),
+        ]);
+        $response->assertForbidden();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_file_must_be_a_file()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        $this->actingAs($submission->doctor);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => 'not a file',
+        ]);
+        $response->assertUnprocessable();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+
+    /** @test */
+    public function test_file_must_be_txt()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        $this->actingAs($submission->doctor);
+        Storage::fake();
+        $response = $this->postJson('api/doctor/prescription/' . $submission->id, [
+            'file' => UploadedFile::fake()->create('prescription.pdf'),
+        ]);
+        $response->assertUnprocessable();
+        Storage::assertMissing('joaquinsommer/' . $response->json('file'));
+    }
+}
